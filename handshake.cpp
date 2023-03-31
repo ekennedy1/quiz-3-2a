00a0c4cf4a6074dcb7a8fdeffa44b2fcc9a41b56
ekennedy1 <ekennedy@tamu.edu>
2023-03-31 00:19:32 +0000

    3-2a

--- handshake.cpp
+++ handshake.cpp
@@ -1,12 +1,58 @@
 #include <iostream>
+#include <mutex> // mutex
+#include <condition_variable>
+#include <fstream>
+#include <thread>
+#include <string> // string
+#include <unistd.h> // getopt
+#include <fstream> // ifstream
+#include <vector> // vector
+#include <stdlib.h> // exit
+#include <chrono> // chrono time
+
+#include <cstring>
+
+using namespace std;
 // include additional necessary headers
 
-void query(/*add necessary parameters*/) {
+condition_variable query_wait;
+condition_variable response_wait;
+mutex m;
+
+
+void query(int c) {
     // Should print: the print number (starting from 0), "SYN", and the three dots "..."
+    unique_lock<mutex> lck(m);
+    //lck.unlock();
+    for(int i = 0; i < c; i++){
+        /*
+        wait(sem_request)
+        print
+        post(sem_response)
+        */
+        query_wait.wait(lck);
+        cout << "[" << i << "] SYN ... ";
+        
+        response_wait.notify_one();
+    }
+    
+    
 }
 
-void response(/*add necessary parameters*/) {
+void response(int count) {
     // Should print "ACK"
+    unique_lock<mutex> lck(m);
+    //lck.unlock();
+    
+    query_wait.notify_one();
+    for(int i = 0; i < count; i++){
+        
+        response_wait.wait(lck);
+        cout << "ACK" << endl;
+        
+        query_wait.notify_all();
+    }
+    
 }
 
 int main(int argc, char** argv) {
@@ -23,6 +69,24 @@ int main(int argc, char** argv) {
      * 4. Provide the threads with necessary args
      * 5. Update the "query" and "response" functions to synchronize the output
     */
+   
+	
+    int count = atoi(argv[1]);
+  
+    //cout << argc << " " << count << endl;
+
+    vector<thread> t_vec;
+
+    
+    
+
+    
+
+    t_vec.push_back(thread(query, count));
+    t_vec.push_back(thread(response, count));
+
+    t_vec.at(0).join();
+    t_vec.at(1).join();
    
     return 0;
 }
